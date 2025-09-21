1590e7308ec6f14ba4f11b8b97c00a54eb5c9782
idk-what-damn <xaraldin.vlad@bk.ru>
2025-09-21 15:48:49 +0300

    Обновил задания: task_5, task_7

--- task_5.py
+++ task_5.py
@@ -1,11 +1,8 @@
 my_integer = int(input("Введите ваше число: "))
-b = []
-while  my_integer > 0:
-    b.append(my_integer % 10)
-    my_integer = my_integer // 10
-    b = b[::1]
-    summ = sum(b)
 
+digits = list(map (int, str(my_integer)))
+
+summ = sum(digits)
 if summ % 7 ==0:
     print("Магическое число!")
 else:
--- task_7.py
+++ task_7.py
@@ -1,3 +1,2 @@
-def palindrom_check(text):
-
-palindrom_string = input("Введите вашу строку: ")
+my_integer = int(input("Введите количество секунд: "))
+print(my_integer // 60, " минут и ", my_integer % 60, "секунд")
