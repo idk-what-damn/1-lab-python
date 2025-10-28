2433f2ff7a42f1b9f4ede857d0eaa5221371ab97
idk-what-damn <xaraldin.vlad@bk.ru>
2025-10-28 14:46:10 +0300

    Изменил задание 7

--- task_7.py
+++ task_7.py
@@ -1,2 +1,2 @@
 my_integer = int(input("Введите количество секунд: "))
-print(my_integer // 60, " минут и ", my_integer % 60, "секунд")
+print(f"{my_integer // 60} минут и {my_integer % 60} секунд")
