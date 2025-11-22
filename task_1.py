a39c9d303ac18bc26208b5a636a87441ba7cd93a
idk-what-damn <xaraldin.vlad@bk.ru>
2025-11-22 14:44:34 +0300

    Исправил решение в 1 задании

--- task_1.py
+++ task_1.py
@@ -1,33 +1,17 @@
 stroka_string = input("Введите вашу строку: ")
-if not stroka_string.split():
+
+if not stroka_string.strip():
     print("Строка пустая!")
 else:
-    stroka_string = stroka_string.lower()
-    words = stroka_string.split()
-    duplicates  = []
-    unique_words = []
-    for i in range(len(words)):
-        count = 0
-        for j in range(len(words)):
-            if words[i] == words[j]:
-                count += 1
-        if count > 1 and words[i] not in duplicates:
-            duplicates.append(words[i])
-        elif count == 1 and words[i] not in unique_words:
-            unique_words.append(words[i])
-    if duplicates:
-        print("Повторяющиеся слова: ")
-        for word in duplicates:
-            count = 0
-            for w in words:
-                if w == word:
-                    count += 1
-            print("Слово:", word, "--- встретилось ", count, "раза")
-    else:
-        print("Повторяющихся слов нет!")
-    if unique_words:
-        print("Уникальные слова: ")
-        for word in unique_words:
-            print( word)
-    else:
-        print("Уникальных слов нет!")
+    words = stroka_string.lower().split()
+
+    word_counts = {}
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    print("\nСловарь слов и их количества:")
+    for word, count in word_counts.items():
+        print(f"{word}: {count}")
+
+    unique_count = len(word_counts)
+    print(f"\nКоличество уникальных слов: {unique_count}")
